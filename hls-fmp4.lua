76119a48566fcb5343749d95eb43ffa6b3e6a96c
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2017-05-05 19:55:41 -0400

    hls-fmp4: remove split() function

--- hls-fmp4.lua
+++ hls-fmp4.lua
@@ -4,30 +4,15 @@ local HLS_LOCATION = "hlsts"
 local DASH_LOCATION = "dash"
 local HLS_FMP4_LOCATION = "hlsfmp4"
 
-function split(inputstr, sep)
-	if sep == nil then
-		sep = "%s"
-	end
-	local t={} ; i=1
-	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-		t[i] = str
-		i = i + 1
-	end
-	return t
-end
-
 function get_init_segment_uri(host, uri)
   local init_segment_header = "#EXT-X-PLAYLIST-TYPE:VOD\n#EXT-X-MAP:URI="
   local init_segment_uri = "http://" .. host .. uri:gsub(HLS_FMP4_LOCATION, DASH_LOCATION):gsub("index","init"):gsub("m3u8", "mp4")
   return init_segment_header .. init_segment_uri
 end
 
--- FIXME I should write a regex instead of splitting
--- from http://localhost/hlsfmp4/drone/drones,360,480,720,1080,p.mp4.urlset/master.m3u8
--- to http://localhost/hlsfmp4/drone/drones360p/index-a1-x3.m3u8
 function get_audio_track(host, uri)
-	local uri_splitted = split(uri, ",")
-	local audio_track_uri = "http://" .. host ..uri_splitted[1] .. uri_splitted[2] .. "p.mp4/index-a1-x3.m3u8"
+  local prefix, level = string.match(uri:gsub(".urlset", ""), "^(.-),(.-),")
+	local audio_track_uri = "http://" .. host .. prefix .. level .. "p.mp4/index-a1-x3.m3u8"
 	return '#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="aac",LANGUAGE="en",NAME="English",DEFAULT=YES,AUTOSELECT=YES,URI="' .. audio_track_uri .. '"\n'
 end
 
